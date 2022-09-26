f3fe818dd4bf54590a6e40d3e0b93b8308ca586a
BishBiswal <112304474+BishBiswal@users.noreply.github.com>
2022-09-25 17:42:18 -0700

    Update ClimbStairs.java

--- ClimbStairs.java
+++ ClimbStairs.java
@@ -19,3 +19,34 @@ class Solution {
         return memo(n, dp);
     }
 }
+
+//Leetcode 746
+
+class Solution {
+    
+    public int memo(int[] cost, int idx, int[] dp){
+        
+        if(idx>=cost.length) return 0;
+        if(dp[idx]!=-1) return dp[idx];
+        
+        
+        int x=cost[idx]+memo(cost,idx+1,dp);
+        int y=cost[idx]+memo(cost,idx+2,dp);
+        
+        return dp[idx]=Math.min(x,y);
+        
+    }
+    
+    
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp=new int[cost.length];
+        Arrays.fill(dp, -1);
+        
+        int a=memo(cost,0, dp);
+        int b=memo(cost,1, dp);
+        return Math.min(a,b);
+        
+    }
+}
+
+
